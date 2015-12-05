a04219438924d9285fd5f3dbdfaafe4d9a73a6a7
Przemyslaw Pastuszka <p.pastuszka@ocado.com>
2015-12-05 18:19:01 +0100

    Trying out another testing strategy

--- src/test/java/CorrectnessTest.java
+++ src/test/java/CorrectnessTest.java
@@ -1,9 +1,4 @@
-import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
-import com.amazonaws.regions.Region;
-import com.amazonaws.regions.Regions;
 import com.amazonaws.services.kinesis.AmazonKinesis;
-import com.amazonaws.services.kinesis.AmazonKinesisClient;
-import com.amazonaws.services.kinesis.model.ShardIteratorType;
 import com.google.cloud.dataflow.sdk.Pipeline;
 import com.google.cloud.dataflow.sdk.io.Read;
 import com.google.cloud.dataflow.sdk.io.TextIO;
@@ -27,10 +22,12 @@ public class CorrectnessTest {
         kinesis.putRecord(streamName, ByteBuffer.wrap("aaa".getBytes("UTF-8")), "0");
 
         Pipeline p = TestPipeline.create();
-        KinesisDataflowSource source = new KinesisDataflowSource(new SimpleKinesisClientProvider(), streamName, ShardIteratorType.TRIM_HORIZON);
+        KinesisDataflowSource source = TestUtils.getTestKinesisSource();
 
-        p.apply(Read.named("kinesis reader").from(source).withMaxNumRecords(5)).apply(ParDo.of(new byteArrayToString())).apply
-                (TextIO.Write.to("/home/ppastuszka/data"));
+        p.apply(Read.named("kinesis reader").from(source).withMaxNumRecords(10)).
+//                apply(Window.<byte[]>into(FixedWindows.of(Duration.standardSeconds(10)))).
+                apply(ParDo.of(new byteArrayToString())).
+                apply(TextIO.Write.to("/home/ppastuszka/data"));
         p.run();
     }
 
---
+++ src/test/java/TestUtils.java
@@ -0,0 +1,32 @@
+import com.amazonaws.services.kinesis.model.ShardIteratorType;
+import com.google.cloud.dataflow.sdk.io.BoundedSource;
+import com.google.cloud.dataflow.sdk.io.UnboundedSource;
+import org.joda.time.Duration;
+import pl.ppastuszka.google.dataflow.kinesis.client.SimpleKinesisClientProvider;
+import pl.ppastuszka.google.dataflow.kinesis.source.KinesisDataflowSource;
+
+import java.lang.reflect.Constructor;
+
+/**
+ * Created by ppastuszka on 05.12.15.
+ */
+public class TestUtils {
+
+    public static KinesisDataflowSource getTestKinesisSource() {
+        return new KinesisDataflowSource(new SimpleKinesisClientProvider(), System.getenv("TEST_KINESIS_STREAM"),
+                ShardIteratorType.TRIM_HORIZON);
+    }
+
+    public static <T, M extends UnboundedSource.CheckpointMark> BoundedSource<T> toBounded(UnboundedSource<T, M> s, long
+            maxRecords) throws Exception {
+        Class<? extends BoundedSource<T>> clazz = (Class<? extends BoundedSource<T>>) Class.forName("com.google.cloud.dataflow" +
+                ".sdk.io" +
+                ".BoundedReadFromUnboundedSource$UnboundedToBoundedSourceAdapter");
+
+        Constructor<? extends BoundedSource<T>> constructor = clazz.getDeclaredConstructor(UnboundedSource.class,
+                Long.TYPE, Duration.class);
+        constructor.setAccessible(true);
+        return constructor.newInstance(s, maxRecords, null);
+    }
+
+}
