b637a979433798c33a4ed8e222a747eca4e36e22
Pastuszka Przemysław <pastuszka.przemyslaw@gmail.com>
2015-12-16 01:39:21 +0100

    integration test working this time

--- src/test/integration/java/CorrectnessIntegrationTest.java
+++ src/test/integration/java/CorrectnessIntegrationTest.java
@@ -7,18 +7,23 @@ import com.google.cloud.dataflow.sdk.values.PCollection;
 
 import org.junit.Test;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 import utils.TestUtils;
 
 /***
  *
  */
 public class CorrectnessIntegrationTest {
+    private ExecutorService singleThreadExecutor = Executors.newSingleThreadExecutor();
+
     @Test
     public void readerTest() throws Exception {
-        final Pipeline p = TestPipeline.create();
-
         List<String> testData = TestUtils.randomStrings(2000);
 
+        final Pipeline p = TestPipeline.create();
         PCollection<String> result = p.
                 apply(Read.
                         from(TestUtils.getTestKinesisSource()).
@@ -26,7 +31,16 @@ public class CorrectnessIntegrationTest {
                 ).
                 apply(ParDo.of(new TestUtils.ByteArrayToString()));
 
-        TestUtils.putRecords(testData);
         DataflowAssert.that(result).containsInAnyOrder(testData);
+
+        Future<?> future = singleThreadExecutor.submit(new Runnable() {
+            @Override
+            public void run() {
+                p.run();
+            }
+        });
+        Thread.sleep(4000);
+        TestUtils.putRecords(testData);
+        future.get(12, TimeUnit.SECONDS);
     }
 }
